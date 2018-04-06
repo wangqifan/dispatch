34614bf5b37b6f13f3d22f18dcc058d5eb41b426
wangqifan <1694675518@qq.com>
2018-04-06 13:35:39 +0800

    12

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import imageio
 import math
 
 
-#---------------pos-----------参数设置
-
 class  PSO():
+    # ---------------pos-----------参数设置
     def __init__(self,PN,dim,max_iter):
+        self.images=[]
         self.w=0.8
         self.c1=2
         self.c2=2
@@ -31,6 +32,18 @@ class  PSO():
         for i in range(length):
             sum+=x[i]
         return sum
+
+    #----------目标函数--griewank----
+    def fitnessFunc(self, chromosome):
+        """F6 Griewank's function
+        multimodal, symmetric, inseparable"""
+        part1 = 0
+        for i in range(len(chromosome)):
+            part1 += chromosome[i] ** 2
+            part2 = 1
+        for i in range(len(chromosome)):
+            part2 *= math.cos(float(chromosome[i]) / math.sqrt(i + 1))
+        return 1 + (float(part1) / 4000.0) - float(part2)
 
     #-----------初始化种群------------
     def init_population(self):
@@ -60,8 +73,6 @@ class  PSO():
         tend=0.4
         k=3.0
         self.w=math.exp(-k*pow(i/self.max_iter,2))*(tmax-tend)+tend
-
-
     #-----------学习因子修改-----
     def getweightofstudy(self,i):
         cstart=2.5
@@ -73,28 +84,41 @@ class  PSO():
     def parameterchange(self,i):
         self.getpsoniw(i)
         self.getweightofstudy(i)
+
+    def scatter(self,i):
+        plt.figure(i)
+        color = ['r', 'y', 'k', 'g', 'm']
+        plt.xlim((-2,2))
+        plt.ylim((-2,2))
+        plt.xlabel("x", size=14)
+        plt.ylabel("y", size=14)
+        plt.scatter(self.X[:,0],self.X[:,1],c=color,linewidths=1)
+        plt.savefig(str(i)+".png")
+        plt.close('all')
+
     #--------------更新粒子位置-------
     def iterator(self):
         fitness=[]
         for t in range(self.max_iter):           #更新gbest pbest
             for i  in range(self.pN):
-                temp=self.function(self.X[i])
+                #temp=self.function(self.X[i])
+                temp=self.fitnessFunc(self.X[i])
                 if(temp<self.p_fit[i]):
                     self.p_fit[i]=temp
                     self.pbest[i]=self.X[i]
                     if self.p_fit[i]<self.fit:
                         self.gbest=self.X[i]
                         self.fit=self.p_fit[i]
-            self.parameterchange(i)
+            self.parameterchange(t)
+            self.scatter(t)
             for i in range(self.pN):
                 self.V[i]=self.w*self.V[i]+self.c1*self.r1*(self.pbest[i]-self.X[i])+self.c2*self.r2*(self.gbest-self.X[i])
                 self.X[i]=self.X[i]+self.V[i]
             fitness.append(self.fit)
-            print(self.fit)
         return fitness
 #---------------------程序执行----------
 def run():
-    my_pso=PSO(PN=30,dim=5,max_iter=20)
+    my_pso=PSO(PN=30,dim=2,max_iter=20)
     my_pso.init_population()
     fitness=my_pso.iterator()
 
